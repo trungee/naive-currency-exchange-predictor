537e9f24ac3f353c7b773bc3abf4628a8f5d9507
unknown <trungnguyent5@HSSS1PCL01366.HarveyNash.vn.local>
2018-07-18 20:14:47 +0700

    Validate input arguments

--- src/main/java/com/github/trungee/coding/naive_currency_exchange_predictor/NaiveCurrencyExchangePredictorApplication.java
+++ src/main/java/com/github/trungee/coding/naive_currency_exchange_predictor/NaiveCurrencyExchangePredictorApplication.java
@@ -11,13 +11,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.github.trungee.coding.naive_currency_exchange_predictor.core.Predictor;
 import com.github.trungee.coding.naive_currency_exchange_predictor.core.Sample;
 import com.github.trungee.coding.naive_currency_exchange_predictor.service.SampleCollector;
+import com.github.trungee.coding.naive_currency_exchange_predictor.validator.ArgumentValidator;
 
 @SpringBootApplication
 public class NaiveCurrencyExchangePredictorApplication implements CommandLineRunner {
 
     @Autowired
     SampleCollector sampleCollector;
-
+    
+    @Autowired
+    ArgumentValidator argumentValidatior;
+    
     private static final String OUTPUT_PATTERN = "The predicted currency exchange from %s to %s for 15/%d/2017 is %f.";
     private static final int PREDICT_MONTH_JANUARY = 1;
     
@@ -27,15 +31,17 @@ public class NaiveCurrencyExchangePredictorApplication implements CommandLineRun
 
     @Override
     public void run(String... args) throws Exception {
-        String exchangeFrom = args[0].substring(args[0].length() - 3, args[0].length());
-        String exchangeTo = args[1].substring(args[1].length() - 3, args[1].length());
-        List<Sample> samples = sampleCollector.collect(exchangeFrom, exchangeTo);
-        Predictor predictor = new Predictor(samples);
-        try {
-            BigDecimal predictedExchangeRate = predictor.predict(PREDICT_MONTH_JANUARY);
-            System.out.println(String.format(OUTPUT_PATTERN, exchangeFrom, exchangeTo, PREDICT_MONTH_JANUARY, predictedExchangeRate.doubleValue()));
-        } catch (Exception e) {
-            System.err.println(e);
+        if (argumentValidatior.validate(args)) {
+            String exchangeFrom = args[0].substring(args[0].length() - 3, args[0].length());
+            String exchangeTo = args[1].substring(args[1].length() - 3, args[1].length());
+            List<Sample> samples = sampleCollector.collect(exchangeFrom, exchangeTo);
+            Predictor predictor = new Predictor(samples);
+            try {
+                BigDecimal predictedExchangeRate = predictor.predict(PREDICT_MONTH_JANUARY);
+                System.out.println(String.format(OUTPUT_PATTERN, exchangeFrom, exchangeTo, PREDICT_MONTH_JANUARY, predictedExchangeRate.doubleValue()));
+            } catch (Exception e) {
+                System.err.println(e);
+            }
         }
     }
 
---
+++ src/main/java/com/github/trungee/coding/naive_currency_exchange_predictor/validator/ArgumentValidator.java
@@ -0,0 +1,28 @@
+package com.github.trungee.coding.naive_currency_exchange_predictor.validator;
+
+import java.util.regex.Pattern;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArgumentValidator {
+
+    private static final Pattern PATTERN = Pattern.compile("\\w*=\\w{3}");
+    
+    public boolean validate(String... args) {
+        boolean isValid = false;
+        if (args != null && args.length >= 2) {
+            if (validateArgument(args[0]) && validateArgument(args[1])) {
+                isValid = true;
+            }
+        }
+        if (!isValid) {
+            throw new IllegalArgumentException("Invalid arguments. Please input argument follow pattern: from={currency} to={currency}");
+        }
+        return isValid;
+    }
+    
+    private boolean validateArgument(String argument) {
+        return PATTERN.matcher(argument).matches();
+    }
+}
